e90454d7d3506bf9b38b4a361306187a1d19b22b
Tim Bruckdorfer <timbruckdorfer@Mac.fritz.box>
2025-10-30 12:03:44 +0100

    Trigger redeployment with OIDC_AUTH_URL secret configured

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from jose import jwt, JWTError
 from jose.utils import base64url_decode
 
-# Updated: Trigger redeployment to pick up new OIDC configuration
+# Trigger redeployment with OIDC_AUTH_URL secret now configured
 app = FastAPI(title="Virtual Patient Backend", version="0.1.0")
 
 # Add CORS middleware
